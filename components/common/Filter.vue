22a8eb63a31c1d05c46a0c252bf4c0248f536b81
Md. Ashraful Islam <maipalash35@gmail.com>
2021-02-07 10:35:48 +0600

    loader added on cities

--- components/common/Filter.vue
+++ components/common/Filter.vue
@@ -2,7 +2,13 @@
     <div>
         <p class="rounded-t-lg p-2 bg-green-1 font-bold">{{ filterTitle }}</p>
         <div class="rounded-b-lg py-2">
-          <div v-for="(filter, i) in filtersData" :key="i">
+          <div :v-if="filtersData" v-for="(filter, i) in filtersData" :key="i">
+            <NuxtLink to="#" class="flex items-center p-2 hover:bg-green-1 duration-300">
+                <i class="ri-arrow-right-s-fill"></i>
+                <p class="font-bold">{{ filter.name }}</p>
+            </NuxtLink>
+          </div>
+          <div :v-if="filtersArea" v-for="(filter, i) in filtersArea" :key="i">
             <NuxtLink to="#" class="flex items-center p-2 hover:bg-green-1 duration-300">
                 <i class="ri-arrow-right-s-fill"></i>
                 <p class="font-bold">{{ filter.area_name }}</p>
@@ -13,6 +19,6 @@
 </template>
 <script>
 export default {
-    props: ['filterTitle','filtersData']
+    props: ['filterTitle','filtersData', 'filtersArea']
 }
 </script>
--- pages/cities.vue
+++ pages/cities.vue
@@ -7,6 +7,8 @@
         <div class="cities">
             <p class="title-color md:text-4xl text-2xl font-black">Where do you want to go ?</p>
             <div style="background-size: cover;" :style="{ backgroundImage: `url(${backgroundImagePath})` }" class="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 lg:gap-6 gap-3 pt-6 pb-12">
+                <loader v-if="isLoading"></loader>
+                <template v-else>
                 <div v-for="(city, i) in cities.data" :key="i">
                     <NuxtLink :to="{name:'markets-city-id', params:{city: city.city_slug, id: city.id }}">
                         <div class="border-2 border-r-4 border-b-4 border-green-4 rounded-xl">
@@ -19,6 +21,7 @@
                         </div>
                     </NuxtLink>
                 </div>
+                </template>
             </div>
         </div>
     </div>
@@ -26,13 +29,16 @@
 <script>
 import Breadcrumb from '~/components/common/Breadcrumb.vue';
 import backgroundImagePath from '~/assets/img/city.png';
+import Loader from '~/components/lib/Loader.vue'
 
 export default {
     components: {
         Breadcrumb,
+        Loader
     },
     data() {
         return {
+          isLoading: true,
           cities: [],
           basePath: this.$axios.defaults.baseURL,
           breadCrumbs: [
@@ -40,6 +46,7 @@ export default {
               {title: 'Go To Market', url: ''},
           ],
           backgroundImagePath,
+
         };
       },
 
@@ -47,6 +54,7 @@ export default {
       this.cities = await fetch(
         this.basePath + '/api/cities'
       ).then(res => res.json())
+      this.isLoading = false
     }
 }
 </script>
