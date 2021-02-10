13b0eaa700d8aa7ccba3d35bf906106b47a59666
Md. Morshed Khan Rana <ra.ranacse@gmail.com>
2021-02-10 16:58:57 +0600

    install nuxt auth for authentication

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default {
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
+  loading: '~/components/LoadingBar.vue',
+
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
@@ -29,7 +31,29 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
+
+  auth: {
+  strategies: {
+    local: {
+      token: {
+        property: 'token',
+        // required: true,
+        // type: 'Bearer'
+      },
+      user: {
+        property: 'user',
+        // autoFetch: true
+      },
+      endpoints: {
+        login: { url: '/api/login', method: 'post' },
+        logout: { url: '/api/logout', method: 'post' },
+        user: { url: '/api/user', method: 'get' }
+      }
+    }
+  }
+},
 
   axios: {
     // proxy: true
