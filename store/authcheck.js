e5dec26b68047892656c7d84d3ad655237f144a4
Md. Morshed Khan Rana <ra.ranacse@gmail.com>
2021-02-16 12:58:38 +0600

    amend

--- store/authcheck.js
+++ store/authcheck.js
@@ -1,50 +1,50 @@
-export const state = {
-  token: this.localStorage.getItem(this.$auth.token),
-  user: JSON.parse(this.localStorage.getItem(this.$auth.user)),
-};
+// export const state = {
+//   token: context.$auth.$storage.getLocalStorage($auth.token),
+//   user: JSON.parse(context.$auth.$storage.getLocalStorage($auth.user)),
+// };
 
-export const getters = {
-  token: (state) => state.token,
-  isLoggedIn(state) {
-    if (state.token) {
-      return true;
-    }
-    return false;
-  },
-  user: (state) => state.user,
-  isUser(state) {
-    if (state.user) {
-      return state.user;
-    }
-    return false;
-  },
-};
+// export const getters = {
+//   token: (state) => state.token,
+//   isLoggedIn(state) {
+//     if (state.token) {
+//       return true;
+//     }
+//     return false;
+//   },
+//   user: (state) => state.user,
+//   isUser(state) {
+//     if (state.user) {
+//       return state.user;
+//     }
+//     return false;
+//   },
+// };
 
-export const actions = {
-  setToken({ commit }, token) {
-    this.localStorage.setItem("en.token", token);
-    commit("SET_TOKEN", token);
-  },
-  removeToken({ commit }) {
-    this.localStorage.removeItem("en.user");
-    this.localStorage.removeItem("en.token");
-    commit("REMOVE_TOKEN");
-  },
-  setUser({ commit }, user) {
-    this.localStorage.setItem("en.user", JSON.stringify(user));
-    commit("SET_USER", user);
-  },
-};
+// export const actions = {
+//   setToken({ commit }, token) {
+//     context.$auth.$storage.setLocalStorage("en.token", token);
+//     commit("SET_TOKEN", token);
+//   },
+//   removeToken({ commit }) {
+//     context.$auth.$storage.removeLocalStorage("en.user");
+//     context.$auth.$storage.removeLocalStorage("en.token");
+//     commit("REMOVE_TOKEN");
+//   },
+//   setUser({ commit }, user) {
+//     context.$auth.$storage.setLocalStorage("en.user", JSON.stringify(user));
+//     commit("SET_USER", user);
+//   },
+// };
 
-export const mutations = {
-  SET_TOKEN(state, token) {
-    state.token = token;
-  },
-  REMOVE_TOKEN(state) {
-    state.token = null;
-    state.user = null;
-  },
-  SET_USER(state, user) {
-    state.user = user;
-  },
-};
+// export const mutations = {
+//   SET_TOKEN(state, token) {
+//     state.token = token;
+//   },
+//   REMOVE_TOKEN(state) {
+//     state.token = null;
+//     state.user = null;
+//   },
+//   SET_USER(state, user) {
+//     state.user = user;
+//   },
+// };
