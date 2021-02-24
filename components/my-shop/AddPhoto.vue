b4ae759c9360c1d04a29ce9e08d2ba66a1d6bb37
Md. Ashraful Islam <maipalash35@gmail.com>
2021-02-24 16:40:47 +0600

    fetch dynamic data for authenticated user & setup custom error page

--- components/my-shop/AddPhoto.vue
+++ components/my-shop/AddPhoto.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="relative pb-1/2">
-            <img class="absolute h-full w-full object-cover rounded-lg" src="~/assets/img/shopping-mall.png" alt="Image">
+        <div class="relative pb-1/2">{{cover_image}}
+            <img class="absolute h-full w-full object-cover rounded-lg" :src="cover_image ? basePath + 'storage/' +  cover_image : require(`~/assets/img/products/default.png`)" alt="Image">
             <button class="absolute left-0 bottom-0 w-10 h-10 items-center border border-white bg-white opacity-50 focus:outline-none rounded-full ml-2 mb-2">
                 <i class="ri-image-add-fill"></i>
             </button>
@@ -10,6 +10,6 @@
 </template>
 <script>
 export default {
-    
+    props:['cover_image']
 }
 </script>
