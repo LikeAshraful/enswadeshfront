54c55f5a9dd8a3a41537c9cb90cb574f33771daf
Imran8599 <imran.fuzzionit@gmail.com>
2021-03-08 15:38:49 +0600

    Search page designed and modal transition added.

---
+++ pages/search/markets.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <markets :cols="cols" :markets="markets" :isLoading="isLoading"></markets>
+    </div>
+</template>
+<script>
+import Markets from '~/components/markets/Markets.vue';
+export default {
+    data(){
+        return {
+            cols: 'lg:grid-cols-4',
+            isLoading:false,
+            markets: [
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+                {shop_count: '12', name: 'Search market name'},
+            ],
+        }
+    },
+    components: {
+        Markets
+    }
+}
+</script>
