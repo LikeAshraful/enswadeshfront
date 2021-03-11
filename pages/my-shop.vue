68e13d463f7b625a6f7c7df56b6ef17c9fc82ced
Imran8599 <imran.fuzzionit@gmail.com>
2021-03-11 13:48:35 +0600

    Solvable spelling

--- pages/my-shop.vue
+++ pages/my-shop.vue
@@ -3,7 +3,7 @@
         <!-- Breadcrumbs -->
         <breadcrumb :breadCrumbs="breadCrumbs"></breadcrumb>
         <div class="flex flex-row gap-6 mb-4">
-            <button @click="shopSubscriptions" :class="subscrip ? 'bg-green-3':''"  class="font-semibold py-1 px-6 border border-gray-3 rounded hover:bg-green-3 delay-100 focus:outline-none">My subscrip</button>
+            <button @click="shopSubscriptions" :class="subscrip ? 'bg-green-3':''"  class="font-semibold py-1 px-6 border border-gray-3 rounded hover:bg-green-3 delay-100 focus:outline-none">My Subscribe</button>
             <button @click="shopRecently" :class="recent ? 'bg-green-3':''"  class="font-semibold py-1 px-6 border border-gray-3 rounded hover:bg-green-3 delay-100 focus:outline-none">Recently Visited</button>
             <button @click="shopFrequently" :class="frequent ? 'bg-green-3':''"  class="font-semibold py-1 px-6 border border-gray-3 rounded hover:bg-green-3 delay-100 focus:outline-none">Frequently Visited</button>
         </div>
