90d50ac61be839542238afe3dcfda11ead5d1e0a
Imran8599 <imran.fuzzionit@gmail.com>
2021-03-16 12:14:21 +0600

    My account page (My Review page designed) and marge solved

--- pages/my-account.vue
+++ pages/my-account.vue
@@ -7,8 +7,9 @@
                 <user-details></user-details>
                 <div class="list mt-4">
                     <ul>
-                        <li v-for="(menu, i) in accountmenu"  :class="$route.path == menu.to ? 'bg-green-3' : ''" class="list-items hover:bg-green-3" :key="'menu'+i"> <NuxtLink   :to="menu.to" >{{menu.title}}</NuxtLink></li>
-
+                        <li v-for="(menu, i) in accountmenu"  :class="$route.path == menu.to ? 'bg-green-3' : ''" class="list-items hover:bg-green-3" :key="'menu'+i"> 
+                            <NuxtLink class="block" :to="menu.to" >{{menu.title}}</NuxtLink>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -24,12 +25,13 @@ import UserDetails from '~/components/my-account/UserDetails.vue';
 export default {
     data() {
         return {
-           accountmenu:[
-             {title:"My Shopping Friends", to:"/my-account/shoppingfriend"},
-             {title:"Shops", to:"/my-account/shops"},
-             {title:"My shops", to:"/my-account/myshops"},
-             {title:"My orders", to:"/my-account/myorders"},
-             {title:"My wishlist", to:"/my-account/mywishlist"},
+            accountmenu:[
+                {title:"My Shopping Friends", to:"/my-account/shoppingfriend"},
+                {title:"Shops", to:"/my-account/shops"},
+                {title:"My shops", to:"/my-account/myshops"},
+                {title:"My orders", to:"/my-account/myorders"},
+                {title:"My wishlist", to:"/my-account/mywishlist"},
+                {title:"My Reviews", to:"/my-account/myreviews"},
            ]
         }
     },
@@ -42,8 +44,3 @@ export default {
 
 }
 </script>
-<style>
-.nuxt-link-active{
-  background: #BAFFD8;
-}
-</style>
