3949679587832822429ec6e48120f11fe0012333
sanwarul <sanwarul.fuzzionit@gmail.com>
2021-03-20 14:55:23 +0600

    Add some pages in my-account

--- pages/my-account/shoppingfriend.vue
+++ pages/my-account/shoppingfriend.vue
@@ -3,35 +3,60 @@
         <p class="h2">My Shopping Friends</p>
         <div class="mt-4 bg-white">
             <div class="p-4 flex">
-                    <NuxtLink v-for="(menu, i) in shopping_friends"
-                      :to="menu.to" :class="$route.path == menu.to ? 'bg-green-3' : ''" class="btn-group hover:bg-green-3" :key="'menu'+i">{{menu.title}}</NuxtLink>
-
+                <div @click="shoppingFriend" :class="shopping_friend ? 'bg-green-3':''" class="btn-group rounded-l hover:bg-green-3">
+                    Friend list
+                </div>
+                <div @click="shoppingFindFriend" :class="shopping_find ? 'bg-green-3':''" class="btn-group hover:bg-green-3">
+                    Find friends
+                </div>
+                <div @click="shoppingFriendRequest" :class="shopping_request ? 'bg-green-3':''" class="btn-group rounded-r hover:bg-green-3">
+                    Friend requests
+                </div>
             </div>
             <div class="divider"></div>
-               <NuxtChild  />
 
+            <friend-list v-if="shopping_friend"></friend-list>
+            <find-friend v-if="shopping_find"></find-friend>
+            <friend-request v-if="shopping_request"></friend-request>
 
         </div>
     </div>
 </template>
 <script>
-
+import FriendList from '~/components/my-account/menus/shopping-friend-items/items/FriendList';
+import FindFriend from '~/components/my-account/menus/shopping-friend-items/items/FindFriend';
+import FriendRequest from '~/components/my-account/menus/shopping-friend-items/items/FriendRequest';
 export default {
     data() {
         return {
-             shopping_friends:[
-             {title:"Friend list", to:"/my-account/shoppingfriend/friendlist"},
-             {title:"Find friends", to:"/my-account/shoppingfriend/findfriend"},
-             {title:"Friend requests", to:"/my-account/shoppingfriend/friendrequest"},
-
-           ]
+            shopping_friend: true,
+            shopping_find: false,
+            shopping_request: false,
         }
     },
-
+    components: {
+        FriendList,
+        FindFriend,
+        FriendRequest,
+    },
 
     methods: {
 
-
+        shoppingFriend(){
+            this.shopping_friend = true;
+            this.shopping_find = false;
+            this.shopping_request = false;
+        },
+        shoppingFindFriend(){
+            this.shopping_friend = false;
+            this.shopping_find = true;
+            this.shopping_request = false;
+        },
+        shoppingFriendRequest(){
+            this.shopping_friend = false;
+            this.shopping_find = false;
+            this.shopping_request = true;
+        },
     }
 }
 </script>
