3aa168d55abbf5ff71c3886a2f084623325e73ec
Md. Ashraful Islam <maipalash35@gmail.com>
2021-03-30 16:11:54 +0600

    order placed doen

--- components/cart/Shipping-billing-address-input.vue
+++ components/cart/Shipping-billing-address-input.vue
@@ -6,6 +6,7 @@
       <input
         id="name"
         type="text"
+        v-model="shipping_name"
         class="input-field focus:outline-none mb-2 mt-1"
         placeholder="Type your name"
       />
@@ -13,39 +14,62 @@
       <label for="email" class="font-semibold">Your email</label>
       <input
         id="email"
+        v-model="shipping_email"
         type="email"
         class="input-field focus:outline-none mb-2 mt-1"
         placeholder="Type your email"
       />
 
       <label for="region" class="font-semibold">Region</label>
-      <select id="region" class="input-field focus:outline-none mb-2 mt-1">
+      <input
+        id="region"
+        v-model="region"
+        type="text"
+        class="input-field focus:outline-none mb-2 mt-1"
+        placeholder="Type your region"
+      >
+      <!-- <select id="region" class="input-field focus:outline-none mb-2 mt-1">
         <option value="" selected disabled>Select your region</option>
         <option value="">...</option>
         <option value="">...</option>
         <option value="">...</option>
-      </select>
+      </select> -->
 
-      <label for="region" class="font-semibold">City</label>
-      <select id="region" class="input-field focus:outline-none mb-2 mt-1">
+      <label for="city" class="font-semibold">City</label>
+      <input
+        id="city"
+        v-model="city"
+        type="text"
+        class="input-field focus:outline-none mb-2 mt-1"
+        placeholder="Type your city"
+      >
+      <!-- <select id="region" class="input-field focus:outline-none mb-2 mt-1">
         <option value="" selected disabled>Select your city</option>
         <option value="">...</option>
         <option value="">...</option>
         <option value="">...</option>
-      </select>
+      </select> -->
 
-      <label for="region" class="font-semibold">Area</label>
-      <select id="region" class="input-field focus:outline-none mb-2 mt-1">
+      <label for="area" class="font-semibold">Area</label>
+      <input
+        id="area"
+        v-model="area"
+        type="email"
+        class="input-field focus:outline-none mb-2 mt-1"
+        placeholder="Type your email"
+      >
+      <!-- <select id="region" class="input-field focus:outline-none mb-2 mt-1">
         <option value="" selected disabled>Select your area</option>
         <option value="">...</option>
         <option value="">...</option>
         <option value="">...</option>
-      </select>
+      </select> -->
 
       <label for="address" class="font-semibold">Address</label>
       <input
         id="address"
         type="text"
+        v-model="address"
         class="border border-gray-3 rounded focus:outline-none px-2 py-1 mt-1 w-full"
         placeholder="Flat# 3B, Floor# 2nd, House#23, Road#1A"
       />
@@ -64,11 +88,11 @@
         <label for="shipping">Same as the shipping address</label>
       </div>
       <div class="flex items-center">
-        <input 
-          @click="anotherAddress" 
-          class="mr-2 w-4 h-4" 
-          type="radio" 
-          name="bill" 
+        <input
+          @click="anotherAddress"
+          class="mr-2 w-4 h-4"
+          type="radio"
+          name="bill"
           id="another" />
         <label for="another">Bill to the another address</label>
       </div>
@@ -110,12 +134,19 @@
         </select>
 
         <label for="region" class="font-semibold">Area</label>
-        <select id="region" class="input-field focus:outline-none mb-2 mt-1">
+        <input
+          id="area"
+          v-model="billing_area"
+          type="email"
+          class="input-field focus:outline-none mb-2 mt-1"
+          placeholder="Type your email"
+        >
+        <!-- <select id="region" class="input-field focus:outline-none mb-2 mt-1">
           <option value="" selected disabled>Select your area</option>
           <option value="">...</option>
           <option value="">...</option>
           <option value="">...</option>
-        </select>
+        </select> -->
 
         <label for="address" class="font-semibold">Address</label>
         <input
@@ -135,7 +166,23 @@ export default {
   data() {
     return {
       billAddress: false,
+      phone_no:'',
+      shipping_name : '',
+      shipping_email : '',
+      region: '',
+      city: '',
+      area: '',
+      address: '',
     }
+  },
+  mounted() {
+    this.phone_no = this.$auth.user.phone,
+    this.shipping_name = this.$auth.user.name
+    this.shipping_email = this.$auth.user.email
+    this.region = this.$auth.user.profile?.region
+    this.city = this.$auth.user.profile?.city
+    this.area = this.$auth.user.profile?.area
+    this.address = this.$auth.user.profile?.address
   },
   methods: {
     anotherAddress(){
