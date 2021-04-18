b9d9b23735b1fcdd6e8a2d37ea908eb6ce682a9a
Imran8599 <imran.fuzzionit@gmail.com>
2021-04-18 15:59:03 +0600

    Tutorial and Calculator page design

--- components/my-shop/tabs/Tutorials.vue
+++ components/my-shop/tabs/Tutorials.vue
@@ -1,10 +1,37 @@
 <template lang="">
     <div>
-        Tutorial tab
+        <p class="title">How to ... ? (5)</p>
+        <div class="p-4 bg-white mb-4">
+            <div class="grid md:grid-cols-4 grid-cols-2 md:gap-4 gap-2">
+                <div v-for="(video, i) in videos" :key="i" class="md:border-2 border border-green-4">
+                    <div class="relative pb-2/3">
+                        <img class="absolute h-full w-full object-cover" src="~/assets/img/products/default.png" alt="Image">
+                        <small class="absolute bottom-0 mb-2 ml-2 text-white bg-green-5 px-3 py-1 inline">10:00</small>
+                    </div>
+                    <div class="p-2">
+                        <p class="font-bold lg:text-lg">Video name goes here</p>
+                    </div>
+                </div>
+            </div>
+            <!-- When no videos yet -->
+            <!-- <div class="py-10 text-center">
+                <p class="h2">You have no videos</p>
+            </div> -->
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            videos: [
+                '',
+                '',
+                '',
+                '',
+                '',
+            ],
+        }
+    }
 }
 </script>
