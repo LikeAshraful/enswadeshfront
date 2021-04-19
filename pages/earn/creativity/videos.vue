772292a9201fff283b51c207f1064ed3dbf30361
Md. Ashraful Islam <maipalash35@gmail.com>
2021-04-19 16:30:37 +0600

    topic api integrated

--- pages/earn/creativity/videos.vue
+++ pages/earn/creativity/videos.vue
@@ -1,20 +1,21 @@
 <template>
     <div>
-        <div v-for="(item, i) in items" :key="i" class="flex justify-between items-center bg-gray-5 p-4 mb-4">
-            <strong>Video topics title will be here</strong>
-            <button @click="uploadModal" class="bg-green-3 font-semibold py-1 px-4 focus:outline-none">Upload</button>
+        <div v-for="(item, i) in topics" :key="i" class="flex justify-between items-center bg-gray-5 p-4 mb-4">
+            <strong>{{item.title}}</strong>
+            <button @click="uploadModal(item.id)" class="bg-green-3 font-semibold py-1 px-4 focus:outline-none">Upload</button>
         </div>
         <div v-if="modal">
-            <div @click="uploadModal" class="fixed inset-0 z-50 flex flex-col justify-center items-center my-12">
+            <div @click="uploadModal(topic.id)" class="fixed inset-0 z-50 flex flex-col justify-center items-center my-12">
                 <div @click="wait">
-                    <div @click="uploadModal" class="w-full text-right pr-1">
+                    <div @click="uploadModal(topic.id)" class="w-full text-right pr-1">
                         <button class="text-2xl text-gray-5 focus:outline-none"><i class="ri-close-line"></i></button>
                     </div>
                     <div class="focus-in max-w-screen-sm shadow-lg bg-white overflow-auto">
                         <div class="">
                             <p class="title">Upload your video</p>
-                            <p class="p-4 font-semibold"><strong>Topics:</strong> Topics title goes to be here.</p>
-                            <div class="p-4 grid md:grid-cols-2 gap-4">
+                            <p class="p-4 font-semibold"><strong>Topics:</strong> {{topic.title}}</p>
+                            <form action="">
+                                <div class="p-4 grid md:grid-cols-2 gap-4">
                                 <div>
                                     <div class="mb-4">
                                         <label class="input-label" for="file">Upload File (MP4, up to 100 MB)</label>
@@ -36,11 +37,12 @@
                                         <textarea class="input-field focus:outline-none" id="references" rows="4" placeholder="Share main video link or others here"></textarea>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="divider"></div>
-                            <div class="p-4 flex items-center justify-center">
-                                <button class="btn-disabled">Submit</button>
-                            </div>
+                                </div>
+                                <div class="divider"></div>
+                                <div class="p-4 flex items-center justify-center">
+                                    <button type="submit" class="btn-disabled">Submit</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -55,28 +57,38 @@ export default {
         return {
             modal: false,
             close_modal: 'modal',
-            items: [
-                '',
-                '',
-                '',
-                '',
-                '',
-            ]
+            topics:[],
+            topic:'',
         }
     },
+    mounted() {
+      this.loadData();
+    },
     methods: {
-        uploadModal()
+      async loadData() {
+        await this.$axios.$get(
+          '/api/topics/category/1'
+        ).then((res) => {
+          this.topics = res.data;
+        })
+      },
+      uploadModal(topicsId)
+      {
+        this.topic = this.topics.filter((obj)=>{
+          return obj.id === topicsId;
+        }).pop();
+
+        if(this.close_modal == 'modal')
         {
-            if(this.close_modal == 'modal')
-            {
-                this.modal = !this.modal;
-            }
-        },
-        wait()
-        {
-            this.close_modal = 'wait';
-            setTimeout(() => this.close_modal = 'modal', 500);
-        },
+            this.modal = !this.modal;
+        }
+      },
+      wait()
+      {
+          this.close_modal = 'wait';
+          setTimeout(() => this.close_modal = 'modal', 500);
+      },
+
     },
 }
 </script>
