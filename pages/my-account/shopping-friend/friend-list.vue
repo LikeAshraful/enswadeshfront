44a854786ef4b7c88656d2f56b318ab1d4f42bb1
Md. Morshed Khan Rana <ra.ranacse@gmail.com>
2021-05-01 17:59:08 +0600

    friend list find friend

--- pages/my-account/shopping-friend/friend-list.vue
+++ pages/my-account/shopping-friend/friend-list.vue
@@ -3,15 +3,27 @@
         <div class="p-4">
             <div class="search">
                 <i class="search-icon ri-search-line"></i>
-                <input type="text"  v-on:keyup.enter="SearchFriendData" v-model="keyword"  class="search-input w-full focus:outline-none" placeholder="Search">
+                <input type="text"  v-on:keyup="searchFriendData" class="search-input w-full focus:outline-none" placeholder="Search">
             </div>
         </div>
 
         <div class="px-4">
-            <div class="grid grid-cols-4 gap-4">
-                <div v-for="(item, i) in followers" :key="i" class="flex flex-col items-center border-2 border-r-4 border-b-4 rounded-lg p-2">
+            <div v-if="sfriends.length > 0" class="grid grid-cols-4 gap-4">
+                <div v-for="(friend, i) in sfriends" :key="i" class="flex flex-col items-center border-2 border-r-4 border-b-4 rounded-lg p-2">
                     <img class="avatar" src="~/assets/img/default_market.png" alt="Image">
-                    <p class="h3 mt-4">{{item.name}}</p>
+                    <p class="h3 mt-4">{{friend.name}}</p>
+                    <div class="w-full mt-4 flex items-center justify-center gap-4 text-xl">
+                        <button class="rounded w-8 h-8 bg-green-1 "><i class="ri-message-2-fill"></i></button>
+                        <button class="rounded w-8 h-8 bg-green-1 "><i class="ri-phone-fill"></i></button>
+                        <button class="rounded w-8 h-8 bg-green-1 "><i class="ri-video-chat-fill"></i></button>
+                    </div>
+                    <button class="mt-4 btn-border hover:bg-green-3 focus:outline-none">Unfriend</button>
+                </div>
+            </div>
+            <div v-else class="grid grid-cols-4 gap-4">
+                <div v-for="(friend, i) in friends" :key="i" class="flex flex-col items-center border-2 border-r-4 border-b-4 rounded-lg p-2">
+                    <img class="avatar" src="~/assets/img/default_market.png" alt="Image">
+                    <p class="h3 mt-4">{{friend.name}}</p>
                     <div class="w-full mt-4 flex items-center justify-center gap-4 text-xl">
                         <button class="rounded w-8 h-8 bg-green-1 "><i class="ri-message-2-fill"></i></button>
                         <button class="rounded w-8 h-8 bg-green-1 "><i class="ri-phone-fill"></i></button>
@@ -27,23 +39,48 @@
     </div>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      keyword:null
+      sfriends: [],
+      friends: []
     }
   },
-  created() {
-    this.FriendData()
+
+  mounted(){
+    this.loadFriends();
   },
-  computed: {
-    ...mapGetters('friend', ['followers']),
-  },
+
   methods: {
-    ...mapActions('friend', ['FriendData']),
-    SearchFriendData(){
-     this.$store.dispatch('friend/SearchFriendData', this.keyword);
+    async searchFriendData(e)
+    {
+      let keyword = e.target.value;
+      if (!keyword) {
+        this.$router.push({path: 'friend-list'});
+        this.friends = [];
+      }else{
+        await this.$axios.get('/api/search/shoping-friend/' + keyword)
+        .then((response) => {
+          this.sfriends = response.data.data;
+        })
+        .catch((error) => {
+          if (error.response.status == 404) {
+            this.$nuxt.error({ statusCode: 404, message: 'err message' })
+          }
+        })
+      }
+    },
+    async loadFriends()
+    {
+      await this.$axios.get('/api/friend-lists')
+      .then((response) => {
+        this.friends = response.data.data;
+      })
+      .catch((error) => {
+        if (error.response.status == 404) {
+          this.$nuxt.error({ statusCode: 404, message: 'err message' })
+        }
+      })
     }
   },
 }
